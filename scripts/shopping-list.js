276b07e62197e2a0632c41469c77c27d31ef8746
Timothy Chang <superfluous11@gmail.com>
2018-08-27 08:25:54 -0700

    completed 2nd namespace exercise

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,4 +1,7 @@
-/* global store, cuid */
+/* global store, cuid, Item */
+'use strict';
+
+
 
 // eslint-disable-next-line no-unused-vars
 const shoppingList = (function() {
@@ -54,8 +57,17 @@ const shoppingList = (function() {
   }
 
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
-  }
+    try{
+      Item.validateName(itemName);
+      const newItem = Item.create(itemName);
+      store.items.push(newItem);
+      render()
+    } catch (error) {
+      console.log(`Cannot add item: ${error.message}`);
+    }
+
+}
+
 
   function handleNewItemSubmit() {
     $('#js-shopping-list-form').submit(function(event) {
