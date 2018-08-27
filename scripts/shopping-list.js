e0bdecc41d75a590e1bb913704a4b7e609629d1d
Timothy Chang <superfluous11@gmail.com>
2018-08-27 08:47:39 -0700

    4th exercise of namespace exercise

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -75,11 +75,6 @@ const shoppingList = (function() {
     });
   }
 
-  function toggleCheckedForListItem(id) {
-    const foundItem = store.items.find(item => item.id === id);
-    foundItem.checked = !foundItem.checked;
-  }
-
   function getItemIdFromElement(item) {
     return $(item)
       .closest('.js-item-element')
@@ -89,19 +84,9 @@ const shoppingList = (function() {
   function handleItemCheckClicked() {
     $('.js-shopping-list').on('click', '.js-item-toggle', event => {
       const id = getItemIdFromElement(event.currentTarget);
-      toggleCheckedForListItem(id);
+      store.findAndToggleChecked(id);
       render();
     });
-  }
-
-  function deleteListItem(id) {
-    const index = store.items.findIndex(item => item.id === id);
-    store.items.splice(index, 1);
-  }
-
-  function editListItemName(id, itemName) {
-    const item = store.items.find(item => item.id === id);
-    item.name = itemName;
   }
 
   function toggleCheckedItemsFilter() {
@@ -118,7 +103,7 @@ const shoppingList = (function() {
       // get the index of the item in store.items
       const id = getItemIdFromElement(event.currentTarget);
       // delete the item
-      deleteListItem(id);
+      store.findAndDelete(id);
       // render the updated shopping list
       render();
     });
@@ -131,7 +116,7 @@ const shoppingList = (function() {
       const itemName = $(event.currentTarget)
         .find('.shopping-item')
         .val();
-      editListItemName(id, itemName);
+      store.findAndUpdateName(id, itemName);
       render();
     });
   }
