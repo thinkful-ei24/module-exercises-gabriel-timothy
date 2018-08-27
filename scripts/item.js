c50966ef4bef4d992fdc06943969066cd3a11eb1
Gabriel Morris <gabemorris@outlook.com>
2018-08-27 08:14:16 -0700

    Item factory and validator

--- scripts/item.js
+++ scripts/item.js
@@ -1,6 +1,23 @@
 'use strict';
-const Item = (function(){
-    const foo = 'bar';
-    return {};
+/* global shoppingList, cuid */
 
+const Item = (function() {
+  const validateName = function(name) {
+    if (!name) {
+      throw new TypeError('Name does not exist');
+    }
+  };
+
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name: name,
+      checked: false
+    };
+  };
+
+  return {
+    validateName: validateName,
+    create: create
+  };
 })();
